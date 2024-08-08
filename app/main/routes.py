05f92c7d89716c9aa7d9b2fce3fb8332ef563ca6
hsnox <161225341+hsnox@users.noreply.github.com>
2024-08-08 08:28:07 -0400

    Added a navigation item

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,3 +6,8 @@ from app.main import bp
 def index():
     print("Index")
     return render_template('index.html')
+
+@bp.route('/development_objectives')
+def development_objectives():
+    print("Development Objectives")
+    return render_template('development_objectives.html')
